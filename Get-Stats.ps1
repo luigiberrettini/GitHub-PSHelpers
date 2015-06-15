4a5e43eb3aef1821d20c0ceff67a304eb694271e
Luigi Berrettini <luigi.berrettini@gmail.com>
2015-06-15 20:43:25 +0200

    List organization member names

--- Get-Stats.ps1
+++ Get-Stats.ps1
@@ -13,3 +13,18 @@ function Write-NumberOfRepos {
   $repos = $global:GITHUB_API_OUTPUT | ? { $($_.owner.login) -eq $Organization -and -not $_.Fork }
   Write-Output 'Number of repos' ($repos | Measure-Object).Count
 }
+
+function Get-OrgMembers {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Organization = $Env:GITHUB_ORG
+  )
+
+  if ([string]::IsNullOrEmpty($Organization))
+    { throw "An organization must be supplied"}
+
+  Get-GitHubOrgMembers -Organization $Organization | Out-Null
+  $global:GITHUB_API_OUTPUT | ? {$_.Name.Length -ne 0 } | % { Write-Output $_.Name }
+}
