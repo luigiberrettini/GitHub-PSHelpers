8193b0ee83a45761a7c956309e84528ee9d97306
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-04-07 15:37:48 +0200

    Add NotRecentlyPushedOrgRepos stat

--- Get-Stats.ps1
+++ Get-Stats.ps1
@@ -28,3 +28,44 @@ function Get-OrgMembers {
   Get-GitHubOrgMembers -Organization $Organization | Out-Null
   $global:GITHUB_API_OUTPUT | ? {$_.Name.Length -ne 0 } | % { Write-Output $_.Name }
 }
+
+function Get-NotRecentlyPushedOrgRepos {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Organization = $Env:GITHUB_ORG,
+
+    [Parameter(Mandatory = $true)]
+    [int]
+    $NumberOfMonthsAgo = 6,
+
+    [Parameter(Mandatory = $false)]
+    [string[]]
+    $ExcludedTeams = @()
+  )
+
+  if ([string]::IsNullOrEmpty($Organization))
+    { throw "An organization must be supplied"}
+
+  Get-GitHubTeams | Out-Null
+  $orgTeams = $global:GITHUB_API_OUTPUT | % { $_.Team.Name }
+
+  $monthsAgo = [DateTime]::Now.AddMonths(-$numberOfMonthsAgo).ToString("u").Replace(" ", "T")
+
+  $notRecentlyPushedRepos = @()
+  $orgTeams |
+    ? { $ExcludedTeams -notcontains $_ } |
+    % {
+      $teamName = $_
+      Get-GitHubTeamRepos -Organization YTech -TeamName $teamName | Out-Null
+      $notRecentlyPushedTeamRepos = $global:GITHUB_API_OUTPUT |
+        ? { $_.pushed_at -le $monthsAgo } |
+        % { $_.name }
+      $notRecentlyPushedRepos += @{ TeamName = $teamName; RepoNames = $notRecentlyPushedTeamRepos }
+    }
+
+  $notRecentlyPushedRepos |
+    ? { $_.RepoNames.Length -gt 0 } |
+    % { Write-Output $_.TeamName $_.RepoNames '' }
+}
